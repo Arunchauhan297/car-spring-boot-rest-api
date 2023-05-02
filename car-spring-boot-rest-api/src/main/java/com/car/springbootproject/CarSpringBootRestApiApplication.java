5fa664701cde5640c08b42b3b5c31b9b7e3d02cc
Arun Chauhan <carun451@gmail.com>
2023-05-02 13:05:20 +0530

    first commit

---
+++ car-spring-boot-rest-api/src/main/java/com/car/springbootproject/CarSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.car.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSpringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ car-spring-boot-rest-api/src/test/java/com/car/springbootproject/CarSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
